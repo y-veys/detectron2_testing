5c4b045999666b7591117376c60ba422de393540
jennie6 <jenniechung6@gmail.com>
2021-05-19 21:11:25 -0600

    redefined image

--- detectron2/data/dataset_mapper.py
+++ detectron2/data/dataset_mapper.py
@@ -303,7 +303,7 @@ class DatasetMapper:
         
         # Add the 4th channel to the image 
         fourth_ch = self.create_prev_box_masks(self.dataset_dicts, dataset_dict) 
-        np.dstack((fourth_ch, image))
+        image = np.dstack((fourth_ch, image))
         print(np.shape(image))
         print(image)
         
