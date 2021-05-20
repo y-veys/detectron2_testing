9dc2d74d6ef7f74c2ef1fc7818bbff2bfd53f365
jennie6 <jenniechung6@gmail.com>
2021-05-19 20:57:19 -0600

    Add self's to box masks fctn

--- detectron2/data/dataset_mapper.py
+++ detectron2/data/dataset_mapper.py
@@ -151,8 +151,8 @@ class DatasetMapper:
         # Parse the image name from the image path by taking the 
         # substring after the last /
         img_name = img_path[img_path.rindex("/")+1:]
-        prev_img_name = curr_to_prev_filename[img_name]
-        prev_img_id = curr_to_prev_img_id[str(img_id)]
+        prev_img_name = self.curr_to_prev_filename[img_name]
+        prev_img_id = self.curr_to_prev_img_id[str(img_id)]
 
         # Find the index of the previous image in the dataset_dicts list 
         i = self.dataset_dicts_index(self.dataset_dicts, prev_img_name)
